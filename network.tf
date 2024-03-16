89e7ab98dfcc0953813344704f2c7156c8c61437
mrdan99 <danielcorreagomes@gmail.com>
2024-03-16 12:51:29 -0300

    modificando subnets

--- network.tf
+++ network.tf
@@ -82,3 +82,48 @@ resource "aws_route_table_association" "private_us_east_1b" {
   
 }
 
+resource "aws_subnet" "private_us_east_1a" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "private_us_east_1a"
+  }
+
+}
+
+resource "aws_subnet" "private_us_east_1b" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name" = "private_us_east_1b"
+  }
+
+}
+
+resource "aws_subnet" "public_us_east_1a" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.64.0/19"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "public_us_east_1a"
+  }
+
+}
+
+resource "aws_subnet" "public_us_east_1b" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.96.0/19"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "public_us_east_1b"
+  }
+
+}
